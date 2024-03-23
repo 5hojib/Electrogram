2fb2f64e68c9a563edfb913a84f359f8fd1ab2dd
5hojib <yesiamshojib@gmail.com>
2024-03-23 23:30:24 +0600

    update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if len(argv) > 1 and argv[1] in ["bdist_wheel", "install", "develop"]:
 
 setup(
     name="pyrogram",
-    version="v176.0",
+    version="v176.1",
     author="5hojib",
     author_email="yesiamshojib@gmail.com",
     description="Nothing",
