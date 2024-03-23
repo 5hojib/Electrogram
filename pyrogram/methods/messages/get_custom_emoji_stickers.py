e935162650f930dc8d560a461fa2dd305943beb1
5hojib <yesiamshojib@gmail.com>
2024-03-23 23:27:35 +0600

    update

--- pyrogram/methods/messages/get_custom_emoji_stickers.py
+++ pyrogram/methods/messages/get_custom_emoji_stickers.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 
 import pyrogram
 from pyrogram import raw
@@ -8,18 +8,21 @@ from pyrogram import types
 class GetCustomEmojiStickers:
     async def get_custom_emoji_stickers(
         self: "pyrogram.Client",
-        custom_emoji_ids: List[int],
-    ) -> List["types.Sticker"]:
+        custom_emoji_ids: Union[int, List[int]],
+    ) -> Union["types.Sticker", List["types.Sticker"]]:
+        is_list = isinstance(custom_emoji_ids, list)
+        custom_emoji_ids = [custom_emoji_ids] if not is_list else custom_emoji_ids
+
         result = await self.invoke(
             raw.functions.messages.GetCustomEmojiDocuments(
                 document_id=custom_emoji_ids
             )
         )
 
-        stickers = []
+        stickers = pyrogram.types.List()
         for item in result:
             attributes = {type(i): i for i in item.attributes}
             sticker = await types.Sticker._parse(self, item, attributes)
             stickers.append(sticker)
 
-        return pyrogram.types.List(stickers)
+        return stickers if is_list else stickers[0]
