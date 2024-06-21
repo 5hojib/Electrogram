ea9e26d95d3b17c851305d5b7059095ce5e90de5
5hojib <yesiamshojib@gmail.com>
2024-06-21 19:27:53 +0600

    update __init__.py

--- pyrogram/methods/users/__init__.py
+++ pyrogram/methods/users/__init__.py
@@ -1,6 +1,5 @@
 from .block_user import BlockUser
 from .delete_profile_photos import DeleteProfilePhotos
-from .delete_stories import DeleteStories
 from .get_chat_photos import GetChatPhotos
 from .get_chat_photos_count import GetChatPhotosCount
 from .get_common_chats import GetCommonChats
@@ -32,6 +31,5 @@ class Users(
     UpdateProfile,
     GetDefaultEmojiStatuses,
     SetEmojiStatus,
-    SendStory
 ):
     pass
