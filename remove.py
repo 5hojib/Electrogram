709f9958eb7589344c8a0d0e6a2cc0e672121a1c
5hojib <yesiamshojib@gmail.com>
2024-07-28 06:43:31 +0000

    Auto-format code [skip actions]

--- remove.py
+++ remove.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 # Define the license comment block to be removed
 license_comment_block = """
@@ -23,24 +22,29 @@ license_comment_block = """
 #  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+
 def remove_license(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path) as file:
         content = file.read()
-    
+
     # Remove the specific license comment block
-    updated_content = content.replace(license_comment_block.strip(), '').strip()
-    
-    with open(file_path, 'w') as file:
+    updated_content = content.replace(
+        license_comment_block.strip(), ""
+    ).strip()
+
+    with open(file_path, "w") as file:
         file.write(updated_content)
+
 
 def remove_from_repo(repo_path):
     for root, _, files in os.walk(repo_path):
         for file in files:
-            if file.endswith('.py'):
+            if file.endswith(".py"):
                 file_path = os.path.join(root, file)
                 remove_license(file_path)
 
+
 if __name__ == "__main__":
     # Path to the specific directory you want to process
-    repo_path = './pyrogram'
+    repo_path = "./pyrogram"
     remove_from_repo(repo_path)
