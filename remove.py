f1bd27b7596a45a175f5314bd8771479db7a9a83
5hojib <yesiamshojib@gmail.com>
2024-07-28 06:48:51 +0000

    Auto-format code [skip actions]

--- remove.py
+++ remove.py
@@ -2,24 +2,7 @@ import os
 
 # Define the license comment block to be removed
 license_comment_block = """
-#  Pyrofork - Telegram MTProto API Client Library for Python
-#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
-#  Copyright (C) 2022-present Mayuri-Chan <https://github.com/Mayuri-Chan>
-#
-#  This file is part of Pyrofork.
-#
-#  Pyrofork is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Lesser General Public License as published
-#  by the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  Pyrofork is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 
 
@@ -42,6 +25,7 @@ def remove_from_repo(repo_path):
             if file.endswith(".py"):
                 file_path = os.path.join(root, file)
                 remove_license(file_path)
+
 
 if __name__ == "__main__":
     # Path to the specific directory you want to process
