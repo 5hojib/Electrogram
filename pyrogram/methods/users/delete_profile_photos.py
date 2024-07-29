f9e580262d533331fd66cfcb7e82c09a608edf29
5hojib <yesiamshojib@gmail.com>
2024-07-29 11:31:53 +0600

    more 234

--- pyrogram/methods/users/delete_profile_photos.py
+++ pyrogram/methods/users/delete_profile_photos.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import pyrogram
 from pyrogram import raw, utils
 from pyrogram.file_id import FileType
--- pyrogram/methods/users/get_default_emoji_statuses.py
+++ pyrogram/methods/users/get_default_emoji_statuses.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import pyrogram
 from pyrogram import raw, types
 
