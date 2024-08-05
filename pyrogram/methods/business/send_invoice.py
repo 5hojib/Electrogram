6d3595cc3ead288cc2f13f5aa29235583b080d44
5hojib <yesiamshojib@gmail.com>
2024-08-05 23:54:44 +0600

    update

--- pyrogram/methods/business/send_invoice.py
+++ pyrogram/methods/business/send_invoice.py
@@ -1,30 +1,29 @@
-from __future__ import annotations
-
 import pyrogram
-from pyrogram import raw, types, utils
 
+from pyrogram import types, raw, utils
+from typing import Union, List
 
 class SendInvoice:
     async def send_invoice(
-        self: pyrogram.Client,
-        chat_id: int | str,
+        self: "pyrogram.Client",
+        chat_id: Union[int, str],
         title: str,
         description: str,
         currency: str,
-        prices: list[types.LabeledPrice],
-        provider: str | None = None,
-        provider_data: str | None = None,
-        payload: str | None = None,
-        photo_url: str | None = None,
-        photo_size: int | None = None,
-        photo_mime_type: str | None = None,
-        start_parameter: str | None = None,
-        extended_media: types.InputMedia = None,
-        reply_to_message_id: int | None = None,
-        message_thread_id: int | None = None,
-        quote_text: str | None = None,
-        quote_entities: list[types.MessageEntity] | None = None,
-        reply_markup: types.InlineKeyboardMarkup = None,
+        prices: Union["types.LabeledPrice", List["types.LabeledPrice"]],
+        provider: str = None,
+        provider_data: str = None,
+        payload: str = None,
+        photo_url: str = None,
+        photo_size: int = None,
+        photo_mime_type: str = None,
+        start_parameter: str = None,
+        extended_media: "types.InputMedia" = None,
+        reply_to_message_id: int = None,
+        message_thread_id: int = None,
+        quote_text: str = None,
+        quote_entities: List["types.MessageEntity"] = None,
+        reply_markup: "types.InlineKeyboardMarkup" = None
     ):
         """Use this method to send invoices.
 
@@ -44,8 +43,10 @@ class SendInvoice:
                 Three-letter ISO 4217 currency code.
                 `XTR` for Telegram Stars.
 
-            prices (List of :obj:`~pyrogram.types.LabeledPrice`):
+            prices (:obj:`~pyrogram.types.LabeledPrice` | List of :obj:`~pyrogram.types.LabeledPrice`):
                 Price with label.
+                If you add multiple prices, the prices will be added up.
+                For stars invoice you can only have one item.
 
             provider (``str``, *optional*):
                 Payment provider.
@@ -97,13 +98,27 @@ class SendInvoice:
         Example:
             .. code-block:: python
 
-                # USD
+                # USD (single prices)
                 app.send_invoice(
                     chat_id,
                     title="Product Name",
                     description="Product Description",
                     currency="USD",
-                    prices=[types.LabeledPrice("Product", 1000)],
+                    prices=types.LabeledPrice("Product", 1000),
+                    provider="Stripe_provider_codes",
+                    provider_data="{}"
+                )
+
+                # USD (multiple prices)
+                app.send_invoice(
+                    chat_id,
+                    title="Product Name",
+                    description="Product Description",
+                    currency="USD",
+                    prices=[
+                        types.LabeledPrice("Product 1", 1000),
+                        types.LabeledPrice("Product 2", 2000)
+                    ],
                     provider="Stripe_provider_codes",
                     provider_data="{}"
                 )
@@ -114,9 +129,11 @@ class SendInvoice:
                     title="Product Name",
                     description="Product Description",
                     currency="XTR",
-                    prices=[types.LabeledPrice("Product", 1000)]
+                    prices=types.LabeledPrice("Product", 1000)
                 )
         """
+
+        is_iterable = not isinstance(prices, types.LabeledPrice)
 
         if reply_markup is not None:
             has_buy_button = False
@@ -131,9 +148,7 @@ class SendInvoice:
                     for price in prices:
                         prices_total += price.amount
                     text = f"Pay ⭐️{prices_total}"
-                reply_markup.inline_keyboard.insert(
-                    0, [types.InlineKeyboardButtonBuy(text=text)]
-                )
+                reply_markup.inline_keyboard.insert(0, [types.InlineKeyboardButtonBuy(text=text)])
 
         reply_to = await utils.get_reply_to(
             client=self,
@@ -141,7 +156,7 @@ class SendInvoice:
             reply_to_message_id=reply_to_message_id,
             message_thread_id=message_thread_id,
             quote_text=quote_text,
-            quote_entities=quote_entities,
+            quote_entities=quote_entities
         )
 
         if payload is not None:
@@ -156,42 +171,38 @@ class SendInvoice:
                     description=description,
                     invoice=raw.types.Invoice(
                         currency=currency,
-                        prices=[price.write() for price in prices],
+                        prices=[price.write() for price in prices] if is_iterable else [prices.write()]
                     ),
                     payload=encoded_payload,
                     provider=provider,
-                    provider_data=raw.types.DataJSON(
-                        data=provider_data if provider_data else "{}"
-                    ),
+                    provider_data=raw.types.DataJSON(data=provider_data if provider_data else "{}"),
                     photo=raw.types.InputWebDocument(
                         url=photo_url,
                         size=photo_size or 0,
                         mime_type=photo_mime_type or "image/jpeg",
-                        attributes=[],
-                    )
-                    if photo_url
-                    else None,
+                        attributes=[]
+                    ) if photo_url else None,
                     start_param=start_parameter,
-                    extended_media=extended_media,
+                    extended_media=extended_media
                 ),
                 random_id=self.rnd_id(),
                 reply_to=reply_to,
                 message="",
-                reply_markup=await reply_markup.write(self)
-                if reply_markup is not None
-                else None,
+                reply_markup=await reply_markup.write(self) if reply_markup is not None else None
             )
         )
 
         for i in r.updates:
             if isinstance(
                 i,
-                raw.types.UpdateNewMessage | raw.types.UpdateNewChannelMessage,
+                (
+                    raw.types.UpdateNewMessage,
+                    raw.types.UpdateNewChannelMessage
+                )
             ):
                 return await types.Message._parse(
                     self,
                     i.message,
                     users={i.id: i for i in r.users},
-                    chats={i.id: i for i in r.chats},
+                    chats={i.id: i for i in r.chats}
                 )
-        return None
