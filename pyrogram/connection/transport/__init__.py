70db530afcb95f1523c672b3fbe0ce7215a065d4
5hojib <yesiamshojib@gmail.com>
2024-08-19 06:49:14 +0600

    update __init__.py

--- pyrogram/connection/transport/__init__.py
+++ pyrogram/connection/transport/__init__.py
@@ -1,21 +1,3 @@
 from __future__ import annotations
 
-from .tcp import (
-    TCP,
-    Proxy,
-    TCPAbridged,
-    TCPAbridgedO,
-    TCPFull,
-    TCPIntermediate,
-    TCPIntermediateO,
-)
-
-__all__ = [
-    "TCP",
-    "Proxy",
-    "TCPAbridged",
-    "TCPAbridgedO",
-    "TCPFull",
-    "TCPIntermediate",
-    "TCPIntermediateO",
-]
+from .tcp import *
