229a0e7703b493bb343ae080371af8c3bbd847a4
5hojib <yesiamshojib@gmail.com>
2024-08-20 20:53:21 +0600

    update story.py

--- pyrogram/types/messages_and_media/story.py
+++ pyrogram/types/messages_and_media/story.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, BinaryIO
+from typing import TYPE_CHECKING, BinaryIO, Union
 
 import pyrogram
 from pyrogram import enums, raw, types, utils
