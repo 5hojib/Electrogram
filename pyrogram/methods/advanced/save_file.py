b904209333edc74d19b01eb8f78c64f09eb1eca6
5hojib <yesiamshojib@gmail.com>
2024-09-07 13:17:00 +0000

    InkyPinkyPonky [no ci]
    Signed-off-by: 5hojib <yesiamshojib@gmail.com>

--- pyrogram/methods/advanced/save_file.py
+++ pyrogram/methods/advanced/save_file.py
@@ -1,27 +1,32 @@
-from asyncio import gather, Queue, sleep
+from __future__ import annotations
+
+from asyncio import Queue, gather, sleep
 from functools import partial
+from hashlib import md5
 from inspect import iscoroutinefunction
+from io import SEEK_END
 from logging import getLogger
 from math import ceil
-from io import SEEK_END
-from hashlib import md5
 from pathlib import PurePath
-from typing import Union, BinaryIO, Callable
+from typing import TYPE_CHECKING, BinaryIO
 
 import pyrogram
 from pyrogram import StopTransmission, raw
 from pyrogram.session import Session
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
 
 log = getLogger(__name__)
 
 
 class SaveFile:
     async def save_file(
-        self: "pyrogram.Client",
-        path: Union[str, BinaryIO],
-        file_id: int = None,
+        self: pyrogram.Client,
+        path: str | BinaryIO,
+        file_id: int | None = None,
         file_part: int = 0,
-        progress: Callable = None,
+        progress: Callable | None = None,
         progress_args: tuple = (),
     ):
         async with self.save_file_semaphore:
@@ -39,7 +44,7 @@ class SaveFile:
                             break
                         except Exception as e:
                             log.warning(f"Retrying part due to error: {e}")
-                            await sleep(2 ** attempt)
+                            await sleep(2**attempt)
 
             def create_rpc(chunk, file_part, is_big, file_id, file_total_parts):
                 if is_big:
@@ -51,38 +56,19 @@ class SaveFile:
                     )
                 else:
                     return raw.functions.upload.SaveFilePart(
-                        file_id=file_id,
-                        file_part=file_part,
-                        bytes=chunk
+                        file_id=file_id, file_part=file_part, bytes=chunk
                     )
 
             part_size = 512 * 1024
             queue = Queue(16)
 
             with (
-                open(
-                    path,
-                    "rb",
-                    buffering=4096
-                )
-                if isinstance(
-                    path,
-                    (
-                        str,
-                        PurePath
-                    )
-                )
+                open(path, "rb", buffering=4096)
+                if isinstance(path, str | PurePath)
                 else path
             ) as fp:
-                file_name = getattr(
-                    fp,
-                    "name",
-                    "file.jpg"
-                )
-                fp.seek(
-                    0,
-                    SEEK_END
-                )
+                file_name = getattr(fp, "name", "file.jpg")
+                fp.seek(0, SEEK_END)
                 file_size = fp.tell()
                 fp.seek(0)
 
@@ -91,7 +77,7 @@ class SaveFile:
 
                 file_size_limit_mib = (
                     4000
-                    if self.me.is_premium # type: ignore
+                    if self.me.is_premium  # type: ignore
                     else 2000
                 )
 
@@ -103,26 +89,17 @@ class SaveFile:
                 file_total_parts = ceil(file_size / part_size)
                 is_big = file_size > 100 * 1024 * 1024
                 pool_size = 2 if is_big else 1
-                workers_count = (
-                    10
-                    if is_big
-                    else 1
-                )
+                workers_count = 10 if is_big else 1
                 is_missing_part = file_id is not None
                 file_id = file_id or self.rnd_id()
-                md5_sum = (
-                    md5()
-                    if not is_big
-                    and not is_missing_part
-                    else None
-                )
+                md5_sum = md5() if not is_big and not is_missing_part else None
 
                 pool = [
                     Session(
                         self,
-                        await self.storage.dc_id(), # type: ignore
-                        await self.storage.auth_key(), # type: ignore
-                        await self.storage.test_mode(), # type: ignore
+                        await self.storage.dc_id(),  # type: ignore
+                        await self.storage.auth_key(),  # type: ignore
+                        await self.storage.test_mode(),  # type: ignore
                         is_media=True,
                     )
                     for _ in range(pool_size)
@@ -139,47 +116,39 @@ class SaveFile:
                         await session.start()
 
                     fp.seek(part_size * file_part)
-                    next_chunk_task = self.loop.create_task(self.preload(
-                        fp,
-                        part_size
-                    ))
+                    next_chunk_task = self.loop.create_task(
+                        self.preload(fp, part_size)
+                    )
 
                     while True:
                         chunk = await next_chunk_task
-                        next_chunk_task = self.loop.create_task(self.preload(
-                            fp,
-                            part_size
-                        ))
+                        next_chunk_task = self.loop.create_task(
+                            self.preload(fp, part_size)
+                        )
 
                         if not chunk:
                             if not is_big and not is_missing_part:
-                                md5_sum = md5_sum.hexdigest() # type: ignore
+                                md5_sum = md5_sum.hexdigest()  # type: ignore
                             break
 
                         await queue.put(
-                            create_rpc(chunk,
-                                       file_part,
-                                       is_big,
-                                       file_id,
-                                       file_total_parts
-                                    )
+                            create_rpc(
+                                chunk, file_part, is_big, file_id, file_total_parts
+                            )
                         )
 
                         if is_missing_part:
-                            return
+                            return None
 
                         if not is_big and not is_missing_part:
-                            md5_sum.update(chunk) # type: ignore
+                            md5_sum.update(chunk)  # type: ignore
 
                         file_part += 1
 
                         if progress and file_part % (file_total_parts // 10) == 0:
                             func = partial(
                                 progress,
-                                min(
-                                    file_part * part_size,
-                                    file_size
-                                ),
+                                min(file_part * part_size, file_size),
                                 file_size,
                                 *progress_args,
                             )
@@ -191,7 +160,10 @@ class SaveFile:
                 except StopTransmission:
                     raise
                 except Exception as e:
-                    log.error(f"Error during file upload at part {file_part}: {e}", exc_info=True)
+                    log.error(
+                        f"Error during file upload at part {file_part}: {e}",
+                        exc_info=True,
+                    )
                 else:
                     if is_big:
                         return raw.types.InputFileBig(
@@ -204,7 +176,7 @@ class SaveFile:
                             id=file_id,
                             parts=file_total_parts,
                             name=file_name,
-                            md5_checksum=md5_sum, # type: ignore
+                            md5_checksum=md5_sum,  # type: ignore
                         )
                 finally:
                     for _ in workers:
