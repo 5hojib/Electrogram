3da9242e1a997e7fcd34ffdf324ef713e5783985
5hojib <yesiamshojib@gmail.com>
2024-09-10 09:07:00 +0600

    update mtproto.py

--- pyrogram/crypto/mtproto.py
+++ pyrogram/crypto/mtproto.py
@@ -52,15 +52,16 @@ def unpack(
     auth_key_id: bytes,
     stored_msg_ids: list[int],
 ) -> Message:
-    SecurityCheckMismatch.check(b.read(8) == auth_key_id)
+    SecurityCheckMismatch.check(b.read(8) == auth_key_id, "b.read(8) == auth_key_id")
 
     msg_key = b.read(16)
     aes_key, aes_iv = kdf(auth_key, msg_key, False)
     data = BytesIO(aes.ige256_decrypt(b.read(), aes_key, aes_iv))
     data.read(8)
 
-    # https://core.telegram.org/mtproto/security_guidelines#checking-session-id
-    SecurityCheckMismatch.check(data.read(8) == session_id)
+    SecurityCheckMismatch.check(
+        data.read(8) == session_id, "data.read(8) == session_id"
+    )
 
     try:
         message = Message.read(data)
@@ -81,40 +82,37 @@ def unpack(
         )
 
     SecurityCheckMismatch.check(
-        msg_key == sha256(auth_key[96 : 96 + 32] + data.getvalue()).digest()[8:24]
+        msg_key == sha256(auth_key[96 : 96 + 32] + data.getvalue()).digest()[8:24],
+        "msg_key == sha256(auth_key[96:96 + 32] + data.getvalue()).digest()[8:24]",
     )
 
-    # https://core.telegram.org/mtproto/security_guidelines#checking-message-length
     data.seek(
         32
-    )  # Get to the payload, skip salt (8) + session_id (8) + msg_id (8) + seq_no (4) + length (4)
+    )
     payload = data.read()
     padding = payload[message.length :]
-    SecurityCheckMismatch.check(12 <= len(padding) <= 1024)
-    SecurityCheckMismatch.check(len(payload) % 4 == 0)
+    SecurityCheckMismatch.check(
+        12 <= len(padding) <= 1024, "12 <= len(padding) <= 1024"
+    )
+    SecurityCheckMismatch.check(len(payload) % 4 == 0, "len(payload) % 4 == 0")
 
-    # https://core.telegram.org/mtproto/security_guidelines#checking-msg-id
-    SecurityCheckMismatch.check(message.msg_id % 2 != 0)
+    SecurityCheckMismatch.check(message.msg_id % 2 != 0, "message.msg_id % 2 != 0")
 
     if len(stored_msg_ids) > STORED_MSG_IDS_MAX_SIZE:
         del stored_msg_ids[: STORED_MSG_IDS_MAX_SIZE // 2]
 
     if stored_msg_ids:
-        # Ignored message: msg_id is lower than all of the stored values
         if message.msg_id < stored_msg_ids[0]:
             raise SecurityCheckMismatch
 
-        # Ignored message: msg_id is equal to any of the stored values
         if message.msg_id in stored_msg_ids:
             raise SecurityCheckMismatch
 
         time_diff = (message.msg_id - MsgId()) / 2**32
 
-        # Ignored message: msg_id belongs over 30 seconds in the future
         if time_diff > 30:
             raise SecurityCheckMismatch
 
-        # Ignored message: msg_id belongs over 300 seconds in the past
         if time_diff < -300:
             raise SecurityCheckMismatch
 
