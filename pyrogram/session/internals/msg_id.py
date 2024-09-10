3b31ec94a4432272128ed21ec8e991c071f27f4c
5hojib <yesiamshojib@gmail.com>
2024-09-10 09:11:51 +0600

    update msg_id.py

--- pyrogram/session/internals/msg_id.py
+++ pyrogram/session/internals/msg_id.py
@@ -1,19 +1,29 @@
 from __future__ import annotations
 
 import logging
-import time
+from datetime import datetime
+from time import perf_counter
 
 log = logging.getLogger(__name__)
 
 
 class MsgId:
+    reference_clock = 0
     last_time = 0
-    offset = 0
+    msg_id_offset = 0
+    server_time = 0
 
     def __new__(cls) -> int:
-        now = int(time.time())
-        cls.offset = (cls.offset + 4) if now == cls.last_time else 0
-        msg_id = (now * 2**32) + cls.offset
+        now = perf_counter() - cls.reference_clock + cls.server_time
+        cls.msg_id_offset = cls.msg_id_offset + 4 if now == cls.last_time else 0
+        msg_id = int(now * 2**32) + cls.msg_id_offset
         cls.last_time = now
 
         return msg_id
+
+    @classmethod
+    def set_server_time(cls, server_time: int):
+        if not cls.server_time:
+            cls.reference_clock = perf_counter()
+            cls.server_time = server_time
+            log.info(f"Time synced: {datetime.utcfromtimestamp(server_time)} UTC")
